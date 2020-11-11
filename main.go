30a34a938f600062bdaa77a6941d2fb2d6c625f5
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-11-11 14:39:00 +0100

    Update bowling game rules example.

--- main.go
+++ main.go
@@ -11,37 +11,52 @@ func main() {
 		// simple game
 		g := bowling.Game{}
 
+		// 5
 		g.Roll(2)
 		g.Roll(3)
 
+		// 5
 		g.Roll(2)
 		g.Roll(3)
 
-		fmt.Println(g.Score()) // 10
+		// 1
+		g.Roll(1)
+
+		fmt.Println(g.Score()) // 11
 	}
 
 	{
 		// spare
 		g := bowling.Game{}
 
+		// 10 + 5
 		g.Roll(8)
 		g.Roll(2)
 
+		// 6
 		g.Roll(5)
 		g.Roll(1)
 
-		fmt.Println(g.Score()) // 21
+		// 1
+		g.Roll(1)
+
+		fmt.Println(g.Score()) // 22
 	}
 
 	{
 		// strike
 		g := bowling.Game{}
 
+		// 10 + 6
 		g.Roll(10)
 
+		// 6
 		g.Roll(5)
 		g.Roll(1)
 
-		fmt.Println(g.Score()) // 22
+		// 1
+		g.Roll(1)
+
+		fmt.Println(g.Score()) // 23
 	}
 }
