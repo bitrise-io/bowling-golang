cee9438a21111f1afb88c359f0c39c8c2bcc3bcd
bejczib <bejczi.balint@gmail.com>
2025-08-29 09:17:57 +0200

    Extend minimal implementation

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -2,13 +2,19 @@ package bowling
 
 // Game ...
 type Game struct {
+	rolls []int
 }
 
 // Roll ...
 func (g *Game) Roll(n int) {
+	g.rolls = append(g.rolls, n)
 }
 
 // Score ...
 func (g *Game) Score() int {
-	return 0
+	score := 0
+	for _, pins := range g.rolls {
+		score += pins
+	}
+	return score
 }
--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -26,6 +26,32 @@ func TestScore(t *testing.T) {
 			},
 			want: 0,
 		},
+		{
+			desc: "simple game",
+			rollAll: func(g *bowling.Game) {
+				g.Roll(3)
+				g.Roll(2)
+
+				g.Roll(4)
+				g.Roll(1)
+
+				g.Roll(2)
+				g.Roll(3)
+			},
+			want: 15,
+		},
+		{
+			desc: "spare",
+			rollAll: func(g *bowling.Game) {
+				g.Roll(7)
+				g.Roll(3) // spare
+				g.Roll(4) // bonus for spare
+				g.Roll(2)
+				g.Roll(1)
+				g.Roll(0)
+			},
+			want: 21, // (7+3+4) + (4+2) + (1+0) = 14 + 6 + 1 = 21
+		},
 	}
 
 	for _, tc := range testCases {
